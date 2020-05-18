72d95bb74aa1abca099142d99bb26f944eb696c4
aman bhardwaj <aman.bhardwaj@3pillarglobal.com>
2020-05-18 10:29:00 +0530

    added routing

---
+++ src/app/details/details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.css']
+})
+export class DetailsComponent{
+
+  constructor() { }
+
+}
---
+++ src/app/error/error.component.ts
@@ -0,0 +1,9 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  template: `<h2>Page you are looking for is fucked up [404] ! </h2>`,
+})
+export class ErrorComponent{
+
+}
